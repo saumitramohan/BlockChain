a04dbbb6b6325aa2a0e6d7c7451d5ec60ff95854
Saumitra Shukla <saumitra90@gmail.com>
2018-04-20 15:55:28 -0700

    Simple block chain - part 1 with mining completed

--- src/main/java/BlockProject/BlockChain/Block.java
+++ src/main/java/BlockProject/BlockChain/Block.java
@@ -12,6 +12,8 @@ public class Block {
 	// Simple Message
 	private String data; 
 	private long timeStamp; 
+	
+	private int nonce;
 
 	//Block Constructor.
 	public Block(String data,String previousHash ) {
@@ -27,8 +29,19 @@ public class Block {
 		String calculatedhash = DigitalSignatureUtil.applyHash( 
 				this.previousHash +
 				Long.toString(timeStamp) +
+				Integer.toString(nonce) +
 				this.data 
 				);
 		return calculatedhash;
 	}
+	
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+	
 }
--- src/test/java/BlockProject/BlockChain/BlockchainTest2.java
+++ src/test/java/BlockProject/BlockChain/BlockchainTest2.java
@@ -13,7 +13,6 @@ public class BlockchainTest2 {
 		blockchain.add(new Block("Genesis Block", "0"));		
 		blockchain.add(new Block("Second Block",blockchain.get(blockchain.size()-1).hash)); 
 		blockchain.add(new Block("Third Block",blockchain.get(blockchain.size()-1).hash));
-		
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
 		System.out.println(blockchainJson);
 	}
